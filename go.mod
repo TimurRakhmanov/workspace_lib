4b38a52a5ef25963e08cd2867be4653f40a246fc
Timur Rakhmanov <rakhmanovtim@gmail.com>
2024-02-24 20:03:28 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TimurRakhmanov/workspace_lib
+
+go 1.21.6
---
+++ lib.go
@@ -0,0 +1,7 @@
+package workspace_lib
+
+// AddNums adds two numbers
+// and returns the result
+func AddNums(a, b int) int {
+	return a + b
+}
